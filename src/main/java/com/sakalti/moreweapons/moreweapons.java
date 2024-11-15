27f260492e2208083170ff47f89ac7314a3d0830
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-15 21:25:06 +0900

    Update moreweapons.java

--- src/main/java/com/sakalti/moreweapons/moreweapons.java
+++ src/main/java/com/sakalti/moreweapons/moreweapons.java
@@ -3,6 +3,7 @@ package com.sakalti.moreweapons;
 import com.sakalti.moreweapons.items.FlowerBladeItem;
 import com.sakalti.moreweapons.items.UnderBladeItem;
 import com.sakalti.moreweapons.items.CoralGreatswordItem;
+import com.sakalti.moreweapons.items.BentDiamondBladeItem;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -15,5 +16,6 @@ public class MoreWeaponsMod implements ModInitializer {
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "flower_blade"), new FlowerBladeItem());
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "under_blade"), new UnderBladeItem());
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "coral_greatsword"), new CoralGreatswordItem());
+        Registry.register(Registries.ITEM, new Identifier("moreweapons", "bent_diamond_blade"), new BentDiamondBladeItem());
     }
 }
