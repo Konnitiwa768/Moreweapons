342edab1488e003e92045e3d94a015e5272ccb83
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-12-03 07:42:43 +0900

    Update moreweapons.java

--- src/main/java/com/sakalti/moreweapons/moreweapons.java
+++ src/main/java/com/sakalti/moreweapons/moreweapons.java
@@ -11,7 +11,10 @@ import com.sakalti.moreweapons.items.ChorusBladeItem;
 import com.sakalti.moreweapons.items.DeepslateSwordItem;
 import com.sakalti.moreweapons.items.NormalRifleItem;
 import com.sakalti.moreweapons.items.ShulkerGreatSwordItem; // シュルカー大剣のインポート
+import com.sakalti.moreweapons.items.DiamondQuarterstaffItem; // ダイヤモンドクアータースタッフのインポート
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -30,8 +33,11 @@ public class MoreWeaponsMod implements ModInitializer {
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "chorus_blade"), new ChorusBladeItem());
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "deepslate_sword"), new DeepslateSwordItem());
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "normal_rifle"), new NormalRifleItem());
-        
-        // シュルカー大剣の登録
         Registry.register(Registries.ITEM, new Identifier("moreweapons", "shulker_great_sword"), new ShulkerGreatSwordItem());
+        
+        // ダイヤモンドクアータースタッフの登録
+        Registry.register(Registries.ITEM, 
+            new Identifier("moreweapons", "diamond_quarterstaff"), 
+            new DiamondQuarterstaffItem(ToolMaterials.DIAMOND, 3, 2.0F, new Item.Settings()));
     }
 }
